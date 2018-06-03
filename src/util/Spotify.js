5dbbdf0c152001499b90266c13568bf28aee3b08
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-06-03 16:13:57 +0300

    feat: implementing spotify search function

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,7 @@
 const CLIENT_ID = "f8cc36e64e5a497bb850446c34c6d7a6";
 const REDIRECT_URL = "http://localhost:3000/";
 const URL_AUTHORIZE = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&state=8597&redirect_uri=${REDIRECT_URL}`;
+const SEARCH_URL = "https://api.spotify.com/v1/search?type=track&q=";
 
 let accessToken = "";
 
@@ -21,6 +22,40 @@ const Spotify = {
         window.location.replace(URL_AUTHORIZE);
       }
     }
+  },
+
+  search: function(term) {
+    const completeSearchURL = SEARCH_URL + term;
+
+    const accessToken = this.getAccessToken();
+
+    const headers = new Headers({
+      Authorization: `Bearer ${accessToken}`
+    });
+
+    const httpRequestData = {
+      method: "GET",
+      headers: headers,
+      mode: "cors",
+      cache: "default"
+    };
+
+    fetch(completeSearchURL, httpRequestData)
+      .then(response => {
+        if (response.status != 200) {
+          console.log(
+            "Looks like there was a problem. Status code: " + response.status
+          );
+          return;
+        }
+
+        response.json().then(data => {
+          console.log(data);
+        });
+      })
+      .catch(err => {
+        console.log("Fetch error: ", err);
+      });
   }
 };
 
